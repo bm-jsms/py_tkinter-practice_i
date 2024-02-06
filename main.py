e7b09b2ff0a13c55624ee6bc49248416fbf28d1c
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-06 18:15:28 -0500

    clean code

--- main.py
+++ main.py
@@ -4,17 +4,14 @@ root = tk.Tk()
 
 root.title("My First GUI with Python")
 
-label = tk.Label(root, text="Hello World!", font=(
-    "Arial", 40), bg="yellow", fg="blue")
+label = tk.Label(root, text="Hello World!", font=("Arial", 40), bg="yellow", fg="blue")
 label.pack(fill=tk.X)
 
 
-label2 = tk.Label(root, text="Title 2", font=(
-    "Arial", 25), bg="green", fg="white")
+label2 = tk.Label(root, text="Title 2", font=("Arial", 25), bg="green", fg="white")
 label2.pack(fill=tk.X)
 
-label3 = tk.Label(root, text="Title 3", font=(
-    "Arial", 25), bg="black", fg="white")
+label3 = tk.Label(root, text="Title 3", font=("Arial", 25), bg="black", fg="white")
 label3.pack(side=tk.LEFT, fill=tk.Y)
 
 
