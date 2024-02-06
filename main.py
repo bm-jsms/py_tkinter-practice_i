b9230cad9ead48b0d51e3d9dbc041bb9404602c6
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-06 18:13:21 -0500

    add - title2

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ label = tk.Label(root, text="Hello World!", font=("Arial", 40), bg="yellow", fg=
 label.pack(fill=tk.X)
 
 
+label2 = tk.Label(root, text="Title 2", font=("Arial", 25), bg="green", fg="white")
+label2.pack(fill=tk.X)
+
+
 def tmp():
     print("\n[INFO] Button was clicked!")
 
