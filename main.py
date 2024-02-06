eac075648dc6044b2c5cabf1a6d6d8fd245880f3
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-06 17:50:10 -0500

    create button

--- main.py
+++ main.py
@@ -7,5 +7,12 @@ root.title("My First GUI with Python")
 label = tk.Label(root, text="Hello World!")
 label.pack()
 
+def tmp():
+    print("\n[INFO] Button was clicked!")
+
+
+button = tk.Button(root, text="Click Me!", command=tmp)
+button.pack()
+
 
 root.mainloop()
